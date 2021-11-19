11fd9f9896b1deef56200cfca6760e11c2f33f5f
realmattclark <matt.clark5991@gmail.com>
2021-11-18 20:52:47 -0600

    main.py update

--- main.py
+++ main.py
@@ -2,43 +2,43 @@ import time
 
 
 if __name__ == '__main__':
+
     hours = int(input('What is the current hour ==> '))
 
     minutes = int(input('What is the current minute ==> '))
 
-    second = int(input('What is the current second ==> '))
+    tick = int(input('What is the current second ==> '))
 
-    class Clock:
-        def __init__(self, hours, minutes, seconds, tick):
-            self.hours = 0
-            self.minutes = 0
-            self.seconds = 0
+
+class Clock:
+
+
+    def __init__(self, hours, minutes, tick):
+        self.hours = 0
+        self.minutes = 0
+        self.tick = 0
     
-        def hours(self):
-            return self.hours
+    def get_hours(self):
+        return self.hours
     
-        def minutes(self):
-            return self.minutes
+    def get_minutes(self):
+        return self.minutes
         
         
-        def tick(self):
-            seconds = 0
-            seconds += 1
-            return self.tick
+    def get_tick(self):
+        seconds = 0
+        seconds += 1
+        return self.tick
 
-        def show(self):
-            print("{02} {02} {02}".format(hours, minutes, tick)
+    def __str__(hours, minutes, tick):
+        print('{02} : {02} : {02}'.format(hours, minutes, tick))
 
         
     
-    clock = Clock(hours, minutes, seconds)
-
-
-def __str__(self, hours, minutes, tick):
-    print('{02} : {02} : {02}'.format(hours, minutes, tick))
+clock = Clock(hours, minutes, tick)
 
 
 while True:
-    print(clock)
+    print(Clock)
     Clock.tick()
     time.sleep(1)
